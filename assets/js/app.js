534132a522727bf70058c3c1624818df9f294558
EstefaniaTamCri <estefania_92_9@hotmail.com>
2023-06-22 16:25:49 +0200

    sin  comitear

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+let icons = document.querySelectorAll("i");
+icons.forEach(icon => {
+  icon.addEventListener("click", () => {
+    icon.style.color = "rgb(255, 195, 17)";
+  });
+});
+
+
+
+
+
+
+
+
